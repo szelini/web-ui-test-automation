ab91448cb39a946650ce6f0b9ffe05fd4226e06b
Szandra Bagyanszki <20890361+szelini@users.noreply.github.com>
2024-03-02 05:11:03 +0100

    Reorganize solution

--- WebUI.TestAutomation/WebUI.TestAutomation.Tests/Tests.cs
+++ WebUI.TestAutomation/WebUI.TestAutomation.Tests/Tests.cs
@@ -2,7 +2,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 
-namespace Tests
+namespace WebUI.TestAutomation.Tests
 {
     [TestFixture]
     public class Tests
